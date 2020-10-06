8faf114ded73bae2456b5a2416419bae7ba3cecd
Jake Simon <jakesimonjs@gmail.com>
2020-10-06 13:24:19 -0400

    Update secondpage.js

--- Assets/java/secondpage.js
+++ Assets/java/secondpage.js
@@ -1,27 +1,15 @@
-$(document).ready(function(){
+$.ajax({
+    url: 'https://api.sportsdata.io/v3/nfl/scores/json/NewsByTeam/sf?key=f7876577a76b4ae49e69a47f4dcdcdff',
+    method: 'GET'
 
-    const url = "https://api.sportsdata.io/v3/nfl/odds/json/AlternateMarketGameOddsByWeek/2020/4?key=f7876577a76b4ae49e69a47f4dcdcdff";
-    // betting odds
-    $.ajax({
-        url: url,
-        method: 'GET'
-    }).then( function(response) {
-        const oddsDiv = document.getElementById('odds');
-        for (i=0;i<15;i++) {
-            var h2tag = document.createElement('h2');
-            var para = document.createElement('p');
-            var homePara = document.createElement('p');
-            h2tag.append(response[i].AwayTeamName+' vs '+response[i].HomeTeamName);
-            para.append(response[i].AwayTeamName+" Money Line = "+ response[i].AlternateMarketPregameOdds[0].AwayMoneyLine);
-            homePara.append(response[i].HomeTeamName+" Money Line = "+ response[i].AlternateMarketPregameOdds[0].HomeMoneyLine);
-            oddsDiv.append(h2tag);
-            oddsDiv.append(para);
-            oddsDiv.append(homePara);
-            console.log(response);
+}).then(function(response){
+    let newsArray = [];
+    for (i=0; i<response.length; i++) {
+        let title = response[i].Title;
+        let content = response[i].Content;
 
-        }
-        
-    });
-    console.log('hi')
+        newsArray.push({title, content});
 
-});
+    }
+    console.log(newsArray);
+})
